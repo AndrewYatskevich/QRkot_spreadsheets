4d92411f9eacbff300a3f23ca3133af46c1b69f2
Andrew Yatskevich <drew.yatskevich@gmail.com>
2023-03-23 00:12:49 +0300

    add google api service

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,9 +1,28 @@
+from datetime import timedelta
+from typing import Union
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.crud.base import CRUDBase
 from app.models import CharityProject
 
 
 class CRUDCharityProject(CRUDBase):
-    pass
+    async def get_projects_by_completion_rate(
+            self,
+            session: AsyncSession,
+    ) -> list[dict[str, Union[str, timedelta]]]:
+        projects = await session.execute(
+            select([
+                CharityProject.name,
+                CharityProject.fundraising_time,
+                CharityProject.description
+            ]).where(
+                CharityProject.fully_invested.is_(True)
+            ).order_by(CharityProject.fundraising_time)
+        )
+        return projects.all()
 
 
 charity_project_crud = CRUDCharityProject(CharityProject)
--- app/models/charity_project.py
+++ app/models/charity_project.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Text
+from sqlalchemy import Column, Interval, String, Text
 
 from app.models.base import CharityProjectDonation
 
@@ -6,3 +6,8 @@ from app.models.base import CharityProjectDonation
 class CharityProject(CharityProjectDonation):
     name = Column(String(100), unique=True, nullable=False)
     description = Column(Text, nullable=False)
+    fundraising_time = Column(Interval, default=None)
+
+    def mark_as_fully_invested(self):
+        super().mark_as_fully_invested()
+        self.fundraising_time = self.close_date - self.create_date
